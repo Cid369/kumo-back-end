32928b8b4681937aa5c7bb2df111069644549fa5
jooewoo <jooewoo@gmail.com>
2019-01-04 15:17:18 -0500

    Fix a typo

--- scripts/collections/update.sh
+++ scripts/collections/update.sh
@@ -10,7 +10,7 @@ curl "${API}${URL_PATH}/${ID}" \
   --header "Authorization: Bearer ${TOKEN}" \
   --data '{
       "collection": {
-        "text": "'"${TEXT}"'",
+        "title": "'"${TITLE}"'",
         "file": "'"${FILE}"'",
         "user": "'"${ID}"'"
       }
