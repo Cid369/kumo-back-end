85b3f7c1da8643b856a5398290d32b8e7fcc3ffe
ecmisra <eshanmisra@gmail.com>
2019-01-04 14:14:00 -0500

    Add collection models
    Create model for adding collection

---
+++ app/models/collection.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const collectionSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  file: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  tag: {
+    type: String
+  }
+
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Collection', collectionSchema)
