0ee0e5a01b65f14bd3abddce35b725db9fcded1e
jooewoo <jooewoo@gmail.com>
2019-01-04 13:55:31 -0500

    changed the database name
    - Changed the database name from kumo-back-end-client to kumo-back-end
      at Mike's suggestion

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'kumo-back-end-client'
+const mongooseBaseName = 'kumo-back-end'
 
 // create the mongodb uri for development and test
 const database = {
